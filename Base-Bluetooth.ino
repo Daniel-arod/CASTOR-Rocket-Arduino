11f4e6fb08dad29236217e6307ba456cd760c49a
Daniel Alejandro Rodriguez <daniel_arod@protonmail.com>
2017-10-02 20:22:03 -0500

    Update Base-Bluetooth.ino

--- Base-Bluetooth.ino
+++ Base-Bluetooth.ino
@@ -16,23 +16,23 @@ void loop(){
  if(Serial.available()>0){
  estado = Serial.read();
  }
- if (estado =='a'){
-   Serial.write(estado);
-   digitalWrite(led13,HIGH);
-   digitalWrite(2,HIGH);
-   digitalWrite(3,LOW);
-   delay(5000);
-   digitalWrite(2,LOW);
-   digitalWrite(3,LOW);
-   delay(1000);
-   digitalWrite(2,LOW);
-   digitalWrite(3,HIGH);
-   delay(5000);
-   digitalWrite(2,LOW);
-   digitalWrite(3,LOW);
-  }
-if(estado=='b'){
-   digitalWrite(led13,LOW);
-   Serial.write(estado);
-  }
-} 
+  despegue(estado);
+}
+void despegue(int x){
+    if (x =='a'){
+     Serial.write(x);
+     digitalWrite(led13,HIGH);//se prende el led, dando señal de que funciona
+     digitalWrite(2,HIGH);//motor
+     digitalWrite(3,LOW);//motor
+     delay(2500);//tiempo que se ejecuta, 2.5 segundos
+     digitalWrite(2,LOW);//se detiene el motor
+     digitalWrite(3,LOW);
+     delay(2500);//tiempo que se ejecuta, 2.5 segundos
+     digitalWrite(2,LOW);//motor gira en sentido inverso
+     digitalWrite(3,HIGH);
+     delay(2500);//tiempo que se ejecuta, 2.5 segundos
+     digitalWrite(2,LOW);//se apaga
+     digitalWrite(3,LOW);
+     x=0;//x se vuelve cero para evita que se vuelva a ejecutar
+    }
+}
