43211234c58e15cd604ef1144d7bccca43ea38b7
Daniel Alejandro Rodriguez <daniel_arod@protonmail.com>
2017-10-04 20:42:36 -0500

    Update paracaidas.ino

--- paracaidas.ino
+++ paracaidas.ino
@@ -1,16 +1,23 @@
+#include<Servo.h>
 #include<Wire.h>
 #include<SPI.h>
 #include <Adafruit_BMP280.h>
 #include <Adafruit_Sensor.h>
 
-float altura,altura_ref,altura_max,altura_minima;
-float presion=549; //la presion local, bogota esta fucking arriba
+Servo servo1;
+float altura = 0  ,altura_ref = 0,altura_max = 0,altura_minima = 0;
+float presion=560; //la presion local, bogota esta fucking arriba
 Adafruit_BMP280 bme;//I2C
+String visual  ;
+String texto = " / ";
 
 void setup(){
+  pinMode(13,OUTPUT);
+  servo1.attach(9);
+  servo1.write(0);
   Serial.begin(9600);
   if(!bme.begin()){
-    Serial.print("error");
+    Serial.println("error");
   }
   delay (500);
   altura_ref=bme.readAltitude(presion);
@@ -26,16 +33,15 @@ void loop(){
     while(!bme.begin());
   }
   altura= bme.readAltitude(presion)-altura_ref;
-    if(altura>=altura_max){
+    if(altura>altura_max){
     altura_max=altura;
   }
-  if(altura<=altura_minima){
-    altura_minima=altura;
-  }
-  Serial.print("\n");
-  Serial.print("altura : ");
-  Serial.print(altura);
+   if (altura_max<=(altura+1)){
+    servo1.write(90);
+    digitalWrite(13,HIGH);
+   }
 
-  delay(100);
-
+  visual = altura + texto + altura_max;  
+   Serial.println(visual);
+  delay(500);
 }
