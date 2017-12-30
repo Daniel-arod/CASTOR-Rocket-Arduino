397332eb69536e329639c2b53526fb3b96e13813
Daniel Alejandro Rodriguez <daniel_arod@protonmail.com>
2017-12-30 11:45:17 -0500

    Update Base-Bluetooth.ino

--- Base-Bluetooth.ino
+++ Base-Bluetooth.ino
@@ -1,39 +1,30 @@
+#include<Servo.h>
 int led13=13;
 int estado=0;
-int estado_anterior=0;
+
+Servo servo1;
+
 void setup(){
   Serial.begin(9600);
   pinMode(led13,OUTPUT);
-  pinMode(2,OUTPUT);
-  pinMode(3,OUTPUT);
+  servo1.attach(9);
+  servo1.write(0);
+ 
 }
 
 void loop(){
- digitalWrite(2,LOW);
- digitalWrite(3,LOW);
  digitalWrite(13,LOW);
 
  if(Serial.available()>0){
  estado = Serial.read();
  }
-  despegue(estado,estado_anterior);
-  estado_anterior+=1;//x se vuelve cero para evita que se vuelva a ejecutar
+  despegue(estado);
+ 
 }
-void despegue(int x,int y){
-    if (x =='a'&& y==0){
+void despegue(int x){
+    if (x =='a'){
      Serial.write(x);
-     digitalWrite(led13,HIGH);//se prende el led, dando señal de que funciona
-     digitalWrite(2,HIGH);//motor
-     digitalWrite(3,LOW);//motor
-     delay(2500);//tiempo que se ejecuta, 2.5 segundos
-     digitalWrite(2,LOW);//se detiene el motor
-     digitalWrite(3,LOW);
-     delay(2500);//tiempo que se ejecuta, 2.5 segundos
-     digitalWrite(2,LOW);//motor gira en sentido inverso
-     digitalWrite(3,HIGH);
-     delay(2500);//tiempo que se ejecuta, 2.5 segundos
-     digitalWrite(2,LOW);//se apaga
-     digitalWrite(3,LOW);
+     digitalWrite(13,HIGH);
+     servo1.write(180);
     }
 }
-
