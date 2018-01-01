37ceaa99c382a5ca132165842f6cff4ae71a1018
Daniel Alejandro Rodriguez <daniel_arod@protonmail.com>
2017-12-31 19:36:58 -0500

    Update paracaidas.ino

--- paracaidas.ino
+++ paracaidas.ino
@@ -10,7 +10,7 @@ float valores_n = 0;  //Es la altura que mide el sensor en un instante k, valore
 float altura_ref = 0; //se hace una medicion y se pone como valor negativo, de tal manera que la diferencia entre los futuros datos
                       // y la altura actual sea la correcta, si no se mueve se mantiene en cero
 int valores_prom = 0; //Son los valores corregidos por el promedio de los datos, seran los que se usaran para las operaciones
-int valores_s = 0;
+float valores_s = 0;
 
 
 //VARIABLES NO USADAS (TEMPORAL):
@@ -19,6 +19,10 @@ int valores_s = 0;
  */
 float presion=560; //la presion local, bogota esta fucking arriba
 Adafruit_BMP280 bme;//I2C
+
+
+//TEXTO QUE SE MUESTRA EN EL SERIAL:
+String texto_completo;
 
 
 void setup(){
@@ -44,16 +48,24 @@ void loop(){
   tierra(); //verifica que el dato no sea menor que cero
   
   int pasado_1 = valores_n;//crea una variable que guarda la altura inmediatamente anterior
+  int correcto_anterior = 0;
   
   if(valores_n-pasado_1>30){//si la diferencia entre la altura actual y la anterior es superior a 30
-    valores_s = (valores_n+pasado_1)/3;//se promedia para equilibrar los datos, el 3 es para que de mas suavidad en el caso de que la diferencia sea muy grande
+      valores_s = (valores_n+pasado_1)/2;//se promedia para equilibrar los datos
+      valores_s = correcto_anterior+ (0.1)*(pasado_1-correcto_anterior);
+      correcto_anterior = valores_s;
   }else{//si no lo es, los valores correctos corresponderan a los valores medidos
-      valores_s = valores_n;
+      valores_s = correcto_anterior+ (0.1)*(pasado_1-correcto_anterior);
+      correcto_anterior = valores_s;
     }
-   
+
+  
+  //texto_completo = "valor actual = "+String(valores_s); 
+
+  
      
-    Serial.println(valores_s); //imprimimos en pantalla las mediciones corregidas  
-    delay(8);//Es lo maximo que puedo usar como tiempo de muestreo, ademas tambien es mas sencillo que el P del PID y da los mismos resultados
+  Serial.println(valores_s/10); //imprimimos en pantalla las mediciones corregidas  
+  delay(8);//Es lo maximo que puedo usar como tiempo de muestreo, ademas tambien es mas sencillo que el P del PID y da los mismos resultados
 
   
     /*if(altura>altura_max){
@@ -85,4 +97,6 @@ void tierra(){ //la gracia de la funcion es que las mediciones, por logica
       valores_n = 0;//siempre sera el cero desde donde se lanza el cohete, no puede ir mas abajo
     }
 }
+
+
 
